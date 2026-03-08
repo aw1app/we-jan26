94f4893e44190e84370b05f959c6fcbd3195b15d
aw1app <aw1app@outlook.com>
2026-03-08 23:18:38 +0530

    react project demo with first component

---
+++ PHASE1/REACT/my-first-react-app/src/components/product/Product.jsx
@@ -0,0 +1,17 @@
+
+
+function Product() {
+    let name = "HP Monitor";
+    let price = 50000;
+
+    // returning JSX
+    return (
+        <div>
+            Name : {name}, Price : {price}
+        </div>
+    )
+}
+
+export default Product;
+
+
