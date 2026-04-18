21c40364fecf8c5caabb996cc4295196de93c498
aw1app <aw1app@outlook.com>
2026-04-18 21:22:34 +0530

    binary search demo

--- PHASE2/SEARCHING/binary-search.js
+++ PHASE2/SEARCHING/binary-search.js
@@ -1,20 +1,19 @@
 
 function binarySearch(arr, target) {
-let n =arr.length;
-  
-    let left = 0; 
-    let right = n-1; 
-    
+    let n = arr.length;
 
-    while(left <= right){
-        let mid = Math.floor ( (left + right) / 2);
+    let left = 0;
+    let right = n - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
 
         if (arr[mid] === target)
             return mid;
 
-        if (target < arr[mid]  ) {
+        if (target < arr[mid]) {
             right = mid - 1;
-        }else{
+        } else {
             left = mid + 1;
         }
     }
@@ -22,11 +21,11 @@ let n =arr.length;
     return -1;
 }
 
-let arr = [2, 24,  45,  66,75, 90, 170, 802];
-let target=170;
+let arr = [2, 24, 45, 66, 75, 90, 170, 802];
+let target = 170;
 
-if (( indexFound =  binarySearch(arr,target) ) !== -1){
-    console.log(target, " found at index ",indexFound);
-}else{
+if ((indexFound = binarySearch(arr, target)) !== -1) {
+    console.log(target, " found at index ", indexFound);
+} else {
     console.log(target, " not found !");
 }
