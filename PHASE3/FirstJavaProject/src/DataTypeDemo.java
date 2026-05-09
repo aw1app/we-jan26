3ab0651e74a239fb619fdc196fbcff00e0cc5cd9
aw1app <aw1app@outlook.com>
2026-05-09 22:09:38 +0530

    Java project demo

---
+++ PHASE3/FirstJavaProject/src/DataTypeDemo.java
@@ -0,0 +1,29 @@
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		byte b1 = 45;
+		
+		short sh1 = 15677;
+		
+		char ch1 = 'h';
+		char ch2 = 'i';
+		
+		int i1= 45454687;
+		
+		long l1 = 34364653462364364L;
+		
+		float f1 = 567.12f;
+		
+		// error because float cannot store this big number
+		//float f2 = 56545454555555666666655555555999999999999999999999995557.12f; 
+		double f2 = 56545454555555666666655555555999999999999999999999995557.12d;
+		
+		
+		System.out.println("f1 = " + f1);
+		System.out.println("f2 = " + f2);
+		System.out.println("End of program");
+
+	}
+
+}
