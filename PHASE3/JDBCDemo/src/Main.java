b75a24647e00119b8861327b182e49fca1b0c339
aw1app <aw1app@outlook.com>
2026-05-30 20:58:19 +0530

    JDBC CRUD demo

--- PHASE3/JDBCDemo/src/Main.java
+++ PHASE3/JDBCDemo/src/Main.java
@@ -18,16 +18,33 @@ public class Main {
         // Loop through all the rows (records) returned by the database.
         System.out.println("ID | NAME         | PRICE     | DESCRIPTION");
         while (resultSet.next()) {
-            // ID | NAME         | PRICE     | DESCRIPTION
 
             int id = resultSet.getInt("ID");
             String name = resultSet.getString("NAME");
             double price = resultSet.getDouble("PRICE");
             String description = resultSet.getString("DESCRIPTION");
 
-
-            System.out.printf("%s | %s  | %s  | %s \n", id, name, price, description);
+            System.out.printf("%d | %s  | %s  | %s \n", id, name, price, description);
         }
+
+
+        // INSERT a new Record demo.
+        //INSERT INTO products(NAME,PRICE,DESCRIPTION) VALUES('NOKIA', 15000.99, 'Great Phone for Elderly');
+        String sqlInsert = "INSERT INTO products(NAME,PRICE,DESCRIPTION) VALUES('NOKIA-2', 15000.99, 'Great Phone for Elderly-2')";
+
+//        int rows = stmt.executeUpdate(sqlInsert);
+//        System.out.printf("Inserted %d row successfully \n!", rows);
+
+        // DELETE demo
+        String sqlDeletePrice700 = "DELETE FROM products WHERE PRICE=12000.99";
+        int rowsDeleted = stmt.executeUpdate(sqlDeletePrice700);
+
+        System.out.printf("Updated %d row(s) successfully! \n", rowsDeleted);
+
+        // Updation demo
+        String sqlUpdateName = "UPDATE products SET NAME='NOKIA - NEW GENERATION' WHERE NAME='NOKIA-2'";
+        int rowsUpdatedCount = stmt.executeUpdate(sqlUpdateName);
+        System.out.printf("Updated %d row(s) successfully! \n", rowsUpdatedCount);
 
         connection.close();
     }
