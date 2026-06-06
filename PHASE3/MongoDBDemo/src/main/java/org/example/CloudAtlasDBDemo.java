bccb744ed1d1a70a8056e4beb78b100550258ae7
aw1app <aw1app@outlook.com>
2026-06-06 22:08:56 +0530

    MongoDB demo part 5 cloud atlas

--- PHASE3/MongoDBDemo/src/main/java/org/example/CloudAtlasDBDemo.java
+++ PHASE3/MongoDBDemo/src/main/java/org/example/CloudAtlasDBDemo.java
@@ -6,7 +6,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-public class AppWithSchemaValidation {
+public class CloudAtlasDBDemo {
     private static MongoClient mongoClient;
     private static MongoDatabase database;
     private static MongoCollection<Document> productsCollection;
@@ -16,29 +16,13 @@ public class AppWithSchemaValidation {
         System.out.println("PRODUCTS IN THE COLLECTIONS");
         listProducts();
 
-        // schema
-        Document priceCriteria = new Document("price", new Document("bsonType", "double")
-                .append("maximum", 100000)
-                .append("description", "price must be a LT 100000 number"));
 
-        Document schemaDoc = new Document("bsonType", "object")
-                .append("properties", priceCriteria);
-
-        Document priceValidation = new Document("$jsonSchema", schemaDoc);
-
-        Document command = new Document("collMod", "products")
-                .append("validator", priceValidation)
-                .append("validationLevel", "moderate")
-                .append("validationAction", "error");
-
-        // enforce schema command
-        database.runCommand(command);
 
         // The insertion should fail because of schema enforcement we did above
         Document prod1 = new Document();
-        prod1.append("id", 17);
-        prod1.append("price", 170000.46f); // price is NOT Max 100000
-        prod1.append("name", "HP AI PC 7");
+        prod1.append("id", 1);
+        prod1.append("price", 7777.46f); // price is NOT Max 100000
+        prod1.append("name", "HP AI PC 77");
 
         try {
             productsCollection.insertOne(prod1);
@@ -57,17 +41,13 @@ public class AppWithSchemaValidation {
     }
 
     private static void connectToMongoDB() {
-
-//        String localDBURL = "mongodb://localhost:27017";
-//        mongoClient = MongoClients.create(localDBURL);
-
-        String cloudDBURL = "mongodb+srv://ciscouser1:RtHUdnGfH8cWUL1s@cluster0.ixxay.mongodb.net/?appName=Cluster0";
+        String cloudDBURL = "mongodb+srv://ciscouser1:<password>@cluster0.ixxay.mongodb.net/?appName=Cluster0&connectTimeoutMS=60000&socketTimeoutMS=60000";
         mongoClient = MongoClients.create(cloudDBURL);
 
 
         database = mongoClient.getDatabase("we_jan_26");
         productsCollection = database.getCollection("products");
-        System.out.println("Successfully connected to products collection of we_jan_26 database");
+        System.out.println("Successfully connected to products collection of we_jan_26 database on the cloud atlas;");
     }
 
 }
