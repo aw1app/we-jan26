72542f8eb958db1f20a94670bdc0c4657e6e2303
aw1app <aw1app@outlook.com>
2026-06-06 19:48:01 +0530

    MongoDB demo part 3 insert/update  collections

--- PHASE3/MongoDBDemo/src/main/java/org/example/Main.java
+++ PHASE3/MongoDBDemo/src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public class Main {
     private static MongoClient mongoClient;
@@ -18,11 +21,15 @@ public class Main {
         listProducts();
 
         Document prod1 = new Document();
-        prod1.append("price",5600.4f);
-        prod1.append("name","HP AI PC 2");
+        prod1.append("id",14);
+        prod1.append("price",30000.46f);
+        prod1.append("name","HP AI PC 4");
 
         System.out.println("INSERTING A NEW PRODUCT IN THE COLLECTIONS");
         insertProduct(prod1);
+
+//        // Update the name
+//        updateProduct("HP AI PC 1", 25.0f);
 
         System.out.println("PRODUCTS IN THE COLLECTIONS AFTER INSERTING THE ABOVE DOC ");
         listProducts();
@@ -49,5 +56,14 @@ public class Main {
 
     private static void insertProduct(Document newProductDoc) {
         productsCollection.insertOne(newProductDoc);
+    }
+
+    private static void updateProduct(String name, float newPrice) {
+        Bson nameFilter = Filters.not(Filters.eq("name", name));
+        Bson update = Updates.set("price",newPrice);
+
+        //productsCollection.updateOne(nameFilter,update );
+        productsCollection.updateMany(nameFilter,update );
+        System.out.println("Product updated successfully:");
     }
 }
