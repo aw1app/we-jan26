fbf320549f32442ad1ffa0df2e17f65e1dcf9174
aw1app <aw1app@outlook.com>
2026-06-28 19:56:07 +0530

    Junit LC methods demo

--- PHASE4/UNIT/JunitDemo/src/test/java/CalculatorTest2.java
+++ PHASE4/UNIT/JunitDemo/src/test/java/CalculatorTest2.java
@@ -1,16 +1,37 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CalculatorTest {
+public class CalculatorTest2 {
+    Calculator calc = null;
+
+    @BeforeAll
+    static void myBeforeAll() {
+        System.out.println(" INSIDE myBeforeAll. This method will run before starting any of the test case  in this class  ");
+    }
+
+    @AfterAll
+    static void myAfterAll() {
+        System.out.println(" INSIDE myAfterAll. This method will run after all test cases have been run  ");
+    }
+
+    @BeforeEach
+    void myBeforeEach() {
+        System.out.println(" INSIDE myBeforeEach. This method will run before each test case in this class  ");
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    void myAfterEach() {
+        System.out.println(" INSIDE myAfterEach. This method will run after each test case in this class  ");
+        calc = null;
+    }
 
     // Req No 1 (Calculator must have the add functionality) test case
     @Test
     void testAdd1(){
-        Calculator calc = new Calculator();
-
+        System.out.println(" INSIDE testAdd1");
         int actualResult = calc.add(2,3);
         int expectedResult = 5;
 
@@ -19,8 +40,7 @@ public class CalculatorTest {
 
     @Test
     void testAdd2(){
-        Calculator calc = new Calculator();
-
+        System.out.println(" INSIDE testAdd2");
         int actualResult = calc.add(-2,-3);
         int expectedResult = -5;
 
@@ -30,8 +50,7 @@ public class CalculatorTest {
     // Req No 2 (Calculator must have the divide functionality) test case
     @Test
     void testDivide1(){
-        Calculator calc = new Calculator();
-
+        System.out.println(" INSIDE testDivide1");
         int actualResult = calc.divide(2,3);
         int expectedResult = 0;
 
@@ -40,8 +59,7 @@ public class CalculatorTest {
 
     @Test
     void testDivide2(){
-        Calculator calc = new Calculator();
-
+        System.out.println(" INSIDE testDivide2");
         int actualResult = calc.divide(0,2);
         int expectedResult = 0;
 
@@ -50,13 +68,7 @@ public class CalculatorTest {
 
     @Test
     void testDivide3(){
-        Calculator calc = new Calculator();
-
-//        int actualResult = calc.divide(2,0);
-//        int expectedResult = 0;
-//
-//        assertEquals( expectedResult, actualResult);
-
+        System.out.println(" INSIDE testDivide3");
         assertThrows(ArithmeticException.class, () -> {
             calc.divide(2,0);
         });
