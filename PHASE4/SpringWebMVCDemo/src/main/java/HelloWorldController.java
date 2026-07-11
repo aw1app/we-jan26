872bfbccc72555327a15fbf0e5ab330d35fb5d43
aw1app <aw1app@outlook.com>
2026-07-11 23:01:07 +0530

    Spring Web MVC starter pom.xml and controller

---
+++ PHASE4/SpringWebMVCDemo/src/main/java/HelloWorldController.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/path1")
+    @ResponseBody
+    public String method1(){
+        return "Hello World";
+    }
+
+}
