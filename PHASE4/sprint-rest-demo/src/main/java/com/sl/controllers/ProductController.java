a529055925c43a5880baf414ba8585feed2a1061
aw1app <aw1app@outlook.com>
2026-07-19 21:02:11 +0530

    Spring REST Server + JPA add and edit product && git push

--- PHASE4/sprint-rest-demo/src/main/java/com/sl/controllers/ProductController.java
+++ PHASE4/sprint-rest-demo/src/main/java/com/sl/controllers/ProductController.java
@@ -2,11 +2,9 @@ package com.sl.controllers;
 
 import com.sl.entities.Product;
 import com.sl.repositries.ProductRepositry;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,26 +17,67 @@ public class ProductController {
     ProductRepositry productRepositry;
 
     @GetMapping("/all")
-    public List<Product> getProducts(){
+    public List<Product> getProducts() {
         List<Product> products = productRepositry.findAll();
 
-        return  products;
+        return products;
     }
 
 
-    @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable("id") int id){
-         Product prod1 = null;
+    @GetMapping("/details/{id}")
+    public Product getProduct(@PathVariable("id") int id) {
+        Product prod1 = null;
 
-        Optional<Product> optionalProduct =  productRepositry.findById(id);
+        Optional<Product> optionalProduct = productRepositry.findById(id);
 
         if (optionalProduct.isPresent())
-            prod1=optionalProduct.get();
+            prod1 = optionalProduct.get();
 
-         return prod1;
+        return prod1;
     }
 
     // TASK-1: Implement /delete/{id}
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") int id) {
+        Optional<Product> optionalProduct = productRepositry.findById(id);
+        if (optionalProduct.isPresent()) {
+            productRepositry.deleteById(id);
+            return "Product found and Deleted";
+        }
+        return "Product not found.";
+    }
+
+    // TASK-2: Implement /add
+    @PostMapping("/add")
+    @Transactional
+    public String addProduct(@RequestBody Product product) {
+        Product productAdded = productRepositry.save(product);
+        if(productAdded!=null)
+        return "Product added succesfully ID=" + productAdded.getId();
+        else
+            return "Product add failed";
+    }
+
+    // Task - 3: /edit/{id}
+    @PostMapping("/edit")
+    @Transactional
+    public String editProduct(@RequestBody Product product) {
+        Optional<Product> optionalProduct = productRepositry.findById(product.getId());
+
+        if (optionalProduct.isPresent()) {
+            Product prod = optionalProduct.get();
+            prod.setName(product.getName());
+            prod.setPrice(product.getPrice());
+            prod.setDescription(product.getDescription());
+
+            productRepositry.save(prod);
+            return "Product updated succesfully ID=" + productAdded.getId();
+        }
+        else
+        return "Product update failed";
+    }
+
 
 //    @GetMapping("/{id}")
 //    public Product getProduct(@PathVariable("id") int id){
@@ -46,7 +85,6 @@ public class ProductController {
 //
 //         return prod1;
 //    }
-
 
 
 }
